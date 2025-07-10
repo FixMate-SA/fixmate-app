a9c13990475b5cafd78a9ae9e06b0ee2c67b607d
FixMate-SA <fixmatesouthafrica@gmail.com>
2025-07-10 02:37:14 +0200

    Corrected media_url_endpoint

--- run.py
+++ run.py
@@ -901,7 +901,7 @@ def whatsapp_webhook():
 
             elif msg_type == 'audio':
                 audio_id = message['audio']['id']
-                media_url_endpoint = f"{DIALOG_360_URL}/v1/media/{audio_id}"
+                media_url_endpoint = f"https://waba-v2.360dialog.io/v1/media/{audio_id}"
                 headers = {'D360-API-KEY': DIALOG_360_API_KEY}
 
                 media_info_response = requests.get(media_url_endpoint, headers=headers)
